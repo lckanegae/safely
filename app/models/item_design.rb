29342cb17f51c4155e631e33074917d25ea60c37
Leon Cohen <cohenleonlbc@gmail.com>
2020-11-12 21:55:48 -0300

    model fixes

--- app/models/item_design.rb
+++ app/models/item_design.rb
@@ -1,10 +1,3 @@
 class ItemDesign < ApplicationRecord
-<<<<<<< HEAD
-<<<<<<< HEAD
   monetize :refund_price
-=======
->>>>>>> Item_design model created
-=======
-  monetize :refund_price
->>>>>>> Gems and migration adaptations
 end

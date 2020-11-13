cefbbad81ee764fff515961ec7b44ba2465655f5
leticiakanegae <lckanegae@gmail.com>
2020-11-12 21:28:35 -0300

    Gems and migration adaptations

--- app/models/item_design.rb
+++ app/models/item_design.rb
@@ -1,6 +1,10 @@
 class ItemDesign < ApplicationRecord
 <<<<<<< HEAD
+<<<<<<< HEAD
   monetize :refund_price
 =======
 >>>>>>> Item_design model created
+=======
+  monetize :refund_price
+>>>>>>> Gems and migration adaptations
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,8 +1,13 @@
 class Subscription < ApplicationRecord
   belongs_to :item_design
 <<<<<<< HEAD
+<<<<<<< HEAD
 
   monetize :price
 =======
 >>>>>>> Subscription model created
+=======
+
+  monetize :price
+>>>>>>> Gems and migration adaptations
 end

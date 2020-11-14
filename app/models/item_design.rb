903cc6d129997fedb8d6096572c653fba3bb8972
leticiakanegae <lckanegae@gmail.com>
2020-11-14 13:42:17 -0300

    Models validations added

--- app/models/item_design.rb
+++ app/models/item_design.rb
@@ -1,3 +1,5 @@
 class ItemDesign < ApplicationRecord
+  has_many :secured_items
+
   monetize :refund_price
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :secured_items
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  validates :age, presence: true
+  validates :cpf, presence: true
 end

f63841b0bb4c0174589b045a6071f1997092d8cb
Leon Cohen <cohenleonlbc@gmail.com>
2020-11-16 12:08:01 -0300

    Removed profile route due to Devise route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
   root to: 'pages#home'
-  get 'profile', to: 'pages#profile'
-  resources :profile, only: [:edit, :update]
   resources :secured_items, only: [:new, :create, :edit, :update] do
     resources :item_designs, only: [:show, :index]
   end

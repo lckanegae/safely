0c47131cf7e41919a3da52e30b4a4d3534ea29eb
Leon Cohen <cohenleonlbc@gmail.com>
2020-11-17 20:46:20 -0300

    Added show and index methods to controller and updated route

--- app/controllers/item_designs_controller.rb
+++ app/controllers/item_designs_controller.rb
@@ -1,7 +1,10 @@
 class ItemDesignsController < ApplicationController
   def show
+    @item_design = ItemDesign.find(params[:id])
+    @subscriptions = Subscription.where(item_design: @item_design)
   end
 
   def index
+    @item_designs = ItemDesign.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
         sessions: 'users/sessions'
       }
   root to: 'pages#home'
-  resources :secured_items, only: [:new, :create, :edit, :update] do
-    resources :item_designs, only: [:show, :index]
-  end
+  resources :item_designs, only: [:show, :index]
+  resources :secured_items, only: [:new, :create, :edit, :update]
 end

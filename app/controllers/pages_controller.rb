8adbc4d41baa1ce1e54f3619c97e567937f293ea
Leon Cohen <cohenleonlbc@gmail.com>
2020-11-20 14:47:07 -0300

    Added Devise whitelisting

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+
   def home
     @top_five_items = SecuredItem.group(:item_design).order(count: :desc).limit(5).count
   end

c2d36f41b831ba942203b348368269e6dde6c329
Leon Cohen <cohenleonlbc@gmail.com>
2020-11-20 11:42:17 -0300

    Added Activate/Deactivate and Insure Again buttons

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,12 +4,12 @@ class PagesController < ApplicationController
   end
 
   def profile
-    @user_items = SecuredItem.where(user: current_user)
-    @expired_item_designs = @user_items.map do |user_item|
+    all_user_items = SecuredItem.where(user: current_user)
+    @expired_items = all_user_items.select do |user_item|
       unless user_item.expiration_date.nil?
-        user_item.item_design if user_item.expiration_date < Time.current
+        user_item
       end
     end
-    @expired_item_designs = @expired_item_designs.uniq
+    @user_items = all_user_items - @expired_items
   end
 end

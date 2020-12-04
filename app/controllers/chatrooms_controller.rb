adab6e211d98782ca014f0ce03d20c0ed009582b
leticiakanegae <lckanegae@gmail.com>
2020-12-03 21:59:57 -0300

    Chatroom nav-icon

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,20 +1,20 @@
 class ChatroomsController < ApplicationController
   def index
     @chatrooms = Chatroom.all
-  end 
-  
+  end
+
   def create
     Chatroom.all.each do |chatroom|
       chat = chatroom.messages.where(user: current_user).first
       if chat.present?
         redirect_to chatroom_path(chatroom)
         return
-      end 
+      end
     end
     new_chatroom = Chatroom.create(name: current_user.nickname)
     redirect_to chatroom_path(new_chatroom)
   end
-  
+
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
